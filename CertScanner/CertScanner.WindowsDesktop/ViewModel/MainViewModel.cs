2c506768c523ffa6baf4c6aa392e508103648ff7
Edi Wang <Edi.Wang@outlook.com>
2018-04-25 14:04:04 +0800

    Update UI

--- CertScanner/CertScanner.WindowsDesktop/ViewModel/MainViewModel.cs
+++ CertScanner/CertScanner.WindowsDesktop/ViewModel/MainViewModel.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using CertScanner.Core;
 using Edi.ExtensionMethods;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace CertScanner.WindowsDesktop.ViewModel
 {
@@ -22,11 +24,13 @@ namespace CertScanner.WindowsDesktop.ViewModel
             set { _systemStoreCerts = value; RaisePropertyChanged(); }
         }
 
+        public RelayCommand CommandOpenCertMgr { get; set; }
+
         public MainViewModel()
         {
             CertificationScanner = new SystemStorageCertificationScanner();
             SystemInfo = $"MachineName: {Environment.MachineName}, OS Version: {Environment.OSVersion}";
-
+            CommandOpenCertMgr = new RelayCommand(() => { Process.Start("certmgr.msc"); });
             SystemStoreCerts = CertificationScanner.ScanCertificates().ToObservableCollection();
         }
     }
