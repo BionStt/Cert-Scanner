b024475c229e9a4516258dd16030da330d4db7bd
Edi Wang <Edi.Wang@outlook.com>
2018-04-27 15:31:51 +0800

    implement context menu

--- CertScanner/CertScanner.Core/CertInfo.cs
+++ CertScanner/CertScanner.Core/CertInfo.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CertScanner.Core.Annotations;
+using GalaSoft.MvvmLight.Command;
 
 namespace CertScanner.Core
 {
-    public class CertInfo
+    public class CertInfo : INotifyPropertyChanged
     {
         public string Subject { get; set; }
 
@@ -25,5 +32,41 @@ namespace CertScanner.Core
         public bool IsExpired => ExpDate < DateTime.Now;
 
         public string Abstract { get; set; }
+
+        public ICommand CertDetail { get; set; }
+
+        public ICommand OpenCertificateLocation { get; set; }
+
+        public CertInfo()
+        {
+            OpenCertificateLocation = new RelayCommand(() =>
+            {
+                var isWinStore = StoreLocation == "CurrentUser" || StoreLocation == "LocalMachine";
+
+                if (isWinStore)
+                {
+                    Process.Start("certmgr.msc");
+                }
+                else
+                {
+                    string argument = "/select, \"" + StoreLocation + "\"";
+                    Process.Start("explorer.exe", argument);
+                }
+            });
+
+            CertDetail = new RelayCommand(() =>
+                {
+                    MessageBox.Show(Abstract, "Certificate Detail", MessageBoxButton.OK,
+                        MessageBoxImage.Information);
+                });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
